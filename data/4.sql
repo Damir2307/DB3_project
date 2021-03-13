84befc51b3f65b44849b060471c20b2ec8435be3
Abzal <55259043+abzal5@users.noreply.github.com>
2021-03-13 18:03:45 +0300

    Delete 4.sql

--- data/4.sql
+++
@@ -1,1 +0,0 @@
-update playground set playgroundName='Green',playgroundAddress='Tole bi 80',price='1500',workTime='9:00 18:00',description='Great stadion',phone='840401600',videoLink='' where playgoundId='1';
